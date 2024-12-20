f51d45f3dcc23379ea15e621631b147780790658
Saskya Panchaud <saskya.panchaud@epfl.ch>
2024-12-20 13:25:25 +0100

    [feat] add /about command + modify commands from BotFather + prompt title, diff of time and all travel options

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 # define start command
 async def start_command(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text("This bot can be used to travel around Switzerland with public transport. To use it, type \"/travel <departure> <destination>\". Let's do it !")
+    await update.message.reply_text("This bot can be used to travel around Switzerland with public transport.\n• type \"/travel <departure> <destination>\" to use it\n• type /about to acess some documentation\nLet's do it !")
+
+# define about command
+async def about_command(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text("Version : 1.0\nSource : https://transport.opendata.ch/")
 
 # define help command
 async def help_command(update: Update, context: CallbackContext) -> None:
@@ -22,7 +26,7 @@ async def help_command(update: Update, context: CallbackContext) -> None:
 async def unknown_command(update: Update, context: CallbackContext) -> None:
     chat = update.effective_chat
     if chat:
-        await context.bot.send_message(chat_id = chat.id, text = "Try \"/travel <departure> <destination>\" or \"/travel <aller_epfl/retour_epfl/aller_heig/retour_heig>\" !")
+        await context.bot.send_message(chat_id = chat.id, text = "Try \"/travel <departure> <destination>\" !")
 
 # define message construction
 async def addToMessage(title, content):
@@ -44,61 +48,73 @@ async def api_call(from_input, to_input):
         return "INVALID TRAVEL"
 
     # message construction
-    message = ""
 
-    # departure
-    departure = "Departure"
-    departure_time = datetime.strptime(json_resp['connections'][0].get('from').get('departure'), '%Y-%m-%dT%H:%M:%S%z').strftime("%X")
-    message += await addToMessage(departure, departure_time)
+    # title
+    message = f"<b>Options to travel between {from_input} and {to_input}</b>\n\n"
 
-    # duration
-    duration = "Duration"
-    duration_time = json_resp['connections'][0].get('duration')
-    message += await addToMessage(duration, duration_time)
+    # options
+    nbOption = 0
+    for connection in json_resp['connections']:
 
-    # step
-    transport = "With"
-    step = "\n"
-    for section in json_resp['connections'][0].get('sections', []):
-        journey = section.get('journey')
-        if journey:
+        # subtitle
+        nbOption += 1
+        message += f"<u>Option {nbOption}</u>\n"
 
-            # construction category + number
-            category = journey['category']
-            number = journey['number']
-            step += f"  - {category}{number} :\n"
+        # departure
+        departure_time = datetime.strptime(connection.get('from').get('departure'), '%Y-%m-%dT%H:%M:%S%z')
+        departure_time_casted = departure_time.strftime("%X")
+        now = datetime.now(departure_time.tzinfo)
+        time_diff = round((departure_time - now).total_seconds() / 60)
+        message += f"Departure in about {time_diff} minutes ({departure_time_casted})\n"
 
-            # construction from
-            from_title = "from"
-            from_name = section.get('departure')['station'].get('name')
-            from_time = datetime.strptime(section.get('departure').get('departure'), '%Y-%m-%dT%H:%M:%S%z').strftime("%X")
-            from_platform = section.get('departure').get('platform')
-            from_platform_content = f" / {from_platform}" if from_platform else ""
-            from_content = f"{from_name}{from_platform_content} - {from_time}"
-            step += await addToStep(from_title, from_content)
-            
-            # construction to
-            to_title = "to"
-            to_name = section.get('arrival')['station'].get('name')
-            to_time = datetime.strptime(section.get('arrival').get('arrival'), '%Y-%m-%dT%H:%M:%S%z').strftime("%X")
-            to_platform = section.get('arrival').get('platform')
-            to_platform_content = f" / {to_platform}" if to_platform else ""
-            to_content = f"{to_name}{to_platform_content} - {to_time}"
-            step += await addToStep(to_title, to_content)
+        # duration
+        duration = "Duration"
+        duration_time = connection.get('duration')
+        message += await addToMessage(duration, duration_time)
 
-            # construction time
-            time = "time"
-            time_elasped = datetime.fromtimestamp(section.get('arrival').get('arrivalTimestamp') - section.get('departure').get('departureTimestamp'), tz=timezone.utc).strftime("%X")
-            step += await addToStep(time, time_elasped)
-        else:
-            # construction walk
-            step += f"  - walk :\n"
-            time = "time"
-            time_elasped = section.get('walk').get('duration') / 60
-            time_elasped_content = f"{time_elasped} minutes"
-            step += await addToStep(time, time_elasped_content)
+        # step
+        transport = "With"
+        step = "\n"
+        for section in connection.get('sections', []):
+            journey = section.get('journey')
+            if journey:
 
-    message += await addToMessage(transport, step)
+                # construction category + number
+                category = journey['category']
+                number = journey['number']
+                step += f"  • {category}{number} :\n"
+
+                # construction from
+                from_title = "from"
+                from_name = section.get('departure')['station'].get('name')
+                from_time = datetime.strptime(section.get('departure').get('departure'), '%Y-%m-%dT%H:%M:%S%z').strftime("%X")
+                from_platform = section.get('departure').get('platform')
+                from_platform_content = f" / {from_platform}" if from_platform else ""
+                from_content = f"{from_name}{from_platform_content} - {from_time}"
+                step += await addToStep(from_title, from_content)
+                
+                # construction to
+                to_title = "to"
+                to_name = section.get('arrival')['station'].get('name')
+                to_time = datetime.strptime(section.get('arrival').get('arrival'), '%Y-%m-%dT%H:%M:%S%z').strftime("%X")
+                to_platform = section.get('arrival').get('platform')
+                to_platform_content = f" / {to_platform}" if to_platform else ""
+                to_content = f"{to_name}{to_platform_content} - {to_time}"
+                step += await addToStep(to_title, to_content)
+
+                # construction time
+                time = "time"
+                time_elasped = datetime.fromtimestamp(section.get('arrival').get('arrivalTimestamp') - section.get('departure').get('departureTimestamp'), tz=timezone.utc).strftime("%X")
+                step += await addToStep(time, time_elasped)
+            else:
+                # construction walk
+                step += f"  • walk :\n"
+                time = "time"
+                time_elasped = section.get('walk').get('duration')
+                time_elasped_content = f"{time_elasped} meters"
+                step += await addToStep(time, time_elasped_content)
+
+        message += await addToMessage(transport, step)
 
     return message
 
@@ -115,35 +131,35 @@ async def travel_command(update: Update, context: CallbackContext):
         to_input = input[1]
 
     message = await api_call(from_input, to_input)
-    await update.message.reply_text(message)
+    await update.message.reply_text(message, parse_mode='HTML')
 
 # define travel from home to epfl command
 async def to_epfl_command(update: Update, context: CallbackContext):
     from_input = "Bottens, croisée"
     to_input = "Ecublens VD, EPFL"
     message = await api_call(from_input, to_input)
-    await update.message.reply_text(message)
+    await update.message.reply_text(message, parse_mode='HTML')
 
 # define travel from epfl to home command
 async def from_epfl_command(update: Update, context: CallbackContext):
     from_input = "Ecublens VD, EPFL"
     to_input = "Bottens, croisée"
     message = await api_call(from_input, to_input)
-    await update.message.reply_text(message)
+    await update.message.reply_text(message, parse_mode='HTML')
 
 # define travel from home to heig command
 async def to_heig_command(update: Update, context: CallbackContext):
     from_input = "Bottens, croisée"
     to_input = "Yverdon-les-Bains, HEIG-VD"
     message = await api_call(from_input, to_input)
-    await update.message.reply_text(message)
+    await update.message.reply_text(message, parse_mode='HTML')
 
 # define travel from heig to home command
 async def from_heig_command(update: Update, context: CallbackContext):
     from_input = "Yverdon-les-Bains, HEIG-VD"
     to_input = "Bottens, croisée"
     message = await api_call(from_input, to_input)
-    await update.message.reply_text(message)
+    await update.message.reply_text(message, parse_mode='HTML')
 
 def main():
     token = BOT_TOKEN
@@ -152,12 +168,13 @@ def main():
     # commands handlers
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("about",about_command))
 
     app.add_handler(CommandHandler("travel", travel_command))
-    app.add_handler(CommandHandler("aller_epfl", to_epfl_command))
-    app.add_handler(CommandHandler("retour_epfl", from_epfl_command))
-    app.add_handler(CommandHandler("aller_heig", to_heig_command))
-    app.add_handler(CommandHandler("retour_heig", from_heig_command))
+    app.add_handler(CommandHandler("bottens_epfl", to_epfl_command))
+    app.add_handler(CommandHandler("epfl_bottens", from_epfl_command))
+    app.add_handler(CommandHandler("bottens_heig", to_heig_command))
+    app.add_handler(CommandHandler("heig_bottens", from_heig_command))
 
     app.add_handler(MessageHandler(filters.COMMAND, unknown_command))
     app.add_handler(MessageHandler(filters.TEXT, unknown_command))
